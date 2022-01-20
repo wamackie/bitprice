5638e89cfcc36ca585268bd070c5d6906fcab430
Wamackie <wamackie8456@gmail.com>
2022-01-20 08:05:08 -0500

    connected api

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,18 @@
 //beginning variables
 
 //constants used
+
 //button section
 const newSignUpBtn = document.querySelector(".sign-up-btn")
 const newSignInBtn = document.querySelector(".sign-in-btn")
 
-//page selection
+//api
+fetch (`https://data.messari.io/api/v1/assets/btc/metrics/market-data`)
+.then(function(response) {
+    return response.json();
+})
+.then(function(data) {
+    console.log(data);
+})
+
+

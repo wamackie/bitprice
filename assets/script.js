752691bbb350e207f38003cf8ac9b7d1e3889650
Wamackie <wamackie8456@gmail.com>
2022-01-20 04:37:22 -0500

    javascript button creation

---
+++ assets/script.js
@@ -0,0 +1,8 @@
+//beginning variables
+
+//constants used
+//button section
+const newSignUpBtn = document.querySelector(".sign-up-btn")
+const newSignInBtn = document.querySelector(".sign-in-btn")
+
+//page selection

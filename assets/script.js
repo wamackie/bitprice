50ef951d6c4f81d6ee5fbf34ecc0d138a71664cc
Wamackie <wamackie8456@gmail.com>
2022-01-20 10:13:23 -0500

    working crypto search console logged

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,32 @@
 //beginning variables
 
 //constants used
-
+var apiKey = `3D704B89-B667-4AD9-81B5-78CE1D33180C`
 //button section
-const newSignUpBtn = document.querySelector(".sign-up-btn")
-const newSignInBtn = document.querySelector(".sign-in-btn")
+const signUpBtn = document.querySelector(".sign-up-btn")
+const signInBtn = document.querySelector(".sign-in-btn")
+const searchBtn = document.querySelector(".searchButton")
+var searchInput = document.getElementById("'search'")
+//variables
 
-//api
-fetch (`https://data.messari.io/api/v1/assets/btc/metrics/market-data`)
-.then(function(response) {
-    return response.json();
-})
-.then(function(data) {
-    console.log(data);
-})
+function signIn() {
+    console.log('Sign in')
+}
+
+function signUp() {
+    console.log('Sign up')
+}
+
+function getInputValue() {
+    console.log(search.value)
+    fetch (`https://data.messari.io/api/v1/assets/${search.value}/metrics/market-data`)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            console.log(data);
+        })
+    }
+
 
 
